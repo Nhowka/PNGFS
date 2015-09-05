4d8a8b2db5ef29d5ec261052c4244a1ec4842626
Diego Esmerio <diego.esmerio@fitsolucoes.biz>
2015-09-05 01:16:35 -0300

    File2PNG: Changed function to save input size and recover byte array from image.

--- PNGFS/File2PNG/Util.cs
+++ PNGFS/File2PNG/Util.cs
@@ -2,32 +2,46 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
 
 namespace File2PNG
 {
     public static class Util
     {
+        public static byte[] FromImage(Image Image)
+        {
+            var RecoveredData = ImageToBytes(Image).ToArray();
+            var FileLengthBytes = RecoveredData.Take(4).ToArray();
+            var FileLength = BitConverter.ToInt32(FileLengthBytes, 0);
+            return RecoveredData.Skip(4).Take(FileLength).ToArray();
+        }
+
         public static Image ToImage(byte[] InputData)
         {
-            var OutputSize = (int)Math.Ceiling(Math.Sqrt(Math.Ceiling(InputData.Length / 3.0)));
+            var InputSize = BitConverter.GetBytes(InputData.Length);
+            var OutputData = new byte[InputSize.Length + InputData.Length];
+            InputSize.CopyTo(OutputData, 0);
+            InputData.CopyTo(OutputData, 4);
+            var OutputSize = (int)Math.Ceiling(Math.Sqrt(Math.Ceiling(OutputData.Length / 3.0)));
             var Image = new Bitmap(OutputSize, OutputSize, PixelFormat.Format24bppRgb);
             var pixel = 0;
             var offset = 0;
             int B = 0, G = 0, R = 0;
             do
             {
-                B = InputData[offset++];
-                G = InputData[offset++];
-                R = InputData[offset++];
+                B = OutputData[offset++];
+                G = OutputData[offset++];
+                R = OutputData[offset++];
                 Image.SetPixel(pixel % OutputSize, pixel / OutputSize, Color.FromArgb(0, R, G, B));
                 ++pixel;
-            } while (offset < InputData.Length - 3);
-            if (offset < InputData.Length)
-                B = InputData[offset++];
-            if (offset < InputData.Length)
-                G = InputData[offset++];
-            if (offset < InputData.Length)
-                R = InputData[offset++];
+            } while (offset < OutputData.Length - 3);
+            if (offset < OutputData.Length)
+                B = OutputData[offset++];
+            if (offset < OutputData.Length)
+                G = OutputData[offset++];
+            if (offset < OutputData.Length)
+                R = OutputData[offset++];
             Image.SetPixel(pixel % OutputSize, pixel / OutputSize, Color.FromArgb(0, R, G, B));
             return Image;
         }
